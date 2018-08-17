dd58d1edadd3617e0e9755f456a9730b6b03daf1
Omar Plata <oommaarr@misena.edu.co>
2018-08-17 18:42:46 -0500

    Commit inicial

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <Page class="page">
+    <ActionBar class="action-bar" title="Home"/>
+
+    <StackLayout>
+      <Button class="btn btn-primary" @tap="$router.push('/counter')">Counter</Button>
+      <Button class="btn btn-primary" @tap="$router.push('/hello')">Hello World</Button>
+      <Label class="body" text="Su puta madre" />
+    </StackLayout>
+
+  </Page>
+</template>
