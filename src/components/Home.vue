543c281450d194f7cd1000d6d899d6dee11878aa
Omar Plata <oommaarr@misena.edu.co>
2018-08-17 18:48:33 -0500

    Cambios

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,6 @@
     <StackLayout>
       <Button class="btn btn-primary" @tap="$router.push('/counter')">Counter</Button>
       <Button class="btn btn-primary" @tap="$router.push('/hello')">Hello World</Button>
-      <Label class="body" text="Su puta madre" />
     </StackLayout>
 
   </Page>
